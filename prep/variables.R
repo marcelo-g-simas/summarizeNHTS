4202a1c3c02171f0232cd16b555fb2a2e70a4e45
cates_a <alexandercates@westat.com>
2017-08-09 12:45:23 -0400

    Reassigned levels for vehicle variables

--- prep/variables.R
+++ prep/variables.R
@@ -10,9 +10,9 @@ variables_2009 <- na.omit(data.table(variables_2009[[1]][,-1]))
 
 data_files <- c("Household File","Person File","Vehicle File","Daytrip File")
 variables_2009[, (data_files) := lapply(.SD, function(x) {x == 'Y'}), .SDcols = data_files]
-variables_2009[, Levels := apply(.SD , 1, function(x) {if(x[1]) 'Household' else if(x[2]) 'Person' else if(x[4]) 'Trip' else 'Vehicle'}), .SDcols = data_files]
+variables_2009[, Levels := apply(.SD , 1, function(x) {if(x[1]) 'Household' else if(x[2]) 'Person' else if(x[3]) 'Vehicle' else 'Trip'}), .SDcols = data_files]
 
-
+variables_2001
 
 #For 2001 variables...
 # Go to http://nhts.ornl.gov/tables09/CodebookBrowser.aspx
@@ -21,10 +21,8 @@ variables_2001 <- read.delim("clipboard", check.names=F)
 setDT(variables_2001)
 data_files <- c("Household File","Person File","Vehicle File","Daytrip File","Longtrip File")
 variables_2001[, (data_files) := lapply(.SD, function(x) {x == 'Y'}), .SDcols = data_files]
-variables_2001[, Levels := apply(.SD , 1, function(x) {if(x[1]) 'Household' else if(x[2]) 'Person' else if(x[4]) 'Trip' else if(x[5]) 'Longtrip' else 'Vehicle'}), .SDcols = data_files]
+variables_2001[, Levels := apply(.SD , 1, function(x) {if(x[1]) 'Household' else if(x[2]) 'Person' else if(x[3]) 'Vehicle' else if(x[4]) 'Trip' else 'Longtrip'}), .SDcols = data_files]
 variables_2001[, Codebook := NULL]
 variables_2001[, `Longtrip File` := NULL]
 variables_2001 <- variables_2001[Levels != 'Longtrip']
-
-variables_2001
 
