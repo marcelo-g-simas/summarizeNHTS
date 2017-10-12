b2514a5d573817280b38c538684a3c6b149131b4
cates_a <alexandercates@westat.com>
2017-10-12 13:15:57 -0400

    added utility functions get_trip_weights, ID, and WGT

--- R/util.R
+++ R/util.R
@@ -1,21 +1,45 @@
+####################################################################################################
+###################################### Configuration Functions #####################################
+####################################################################################################
+#' @export
+# ID
+ID <- function(level) {
+  id <- switch(
+    EXPR = level,
+    household = 'HOUSEID',
+    person = 'PERSONID',
+    trip = 'TDTRPNUM',
+    vehicle = 'VEHID'
+  )
+  if(is.null(id)) stop(level,' is not a valid ID level.')
+  return(id)
+}
+
 #==================================================================================================#
 #' @export
-#get_wgt_names
-get_wgt_names <- function(wgt_name) {
-
-  # if(!wgt_name %in% c('HHWGT','WTPERFIN','DAYWGT','SFWGT')) {
-  #   stop(sprintf('%s is not a valid weight column name/prefix. Use "HHWGT", "WTPERFIN", "DAYWGT", or "SFWGT"',wgt_name))
-  # }
-
-  return(c(wgt_name, paste0(wgt_name, 1:100)))
-
+# WGT
+WGT <- function(level) {
+  wgt <- switch(
+    EXPR = level,
+    household = 'HHWGT',
+    person = 'WTPERFIN',
+    trip = 'DAYWGT'
+  )
+  if(is.null(wgt)) stop(level,' is not a valid weight level.')
+  replicates <- paste0(wgt, 1:100)
+  return(c(wgt, replicates))
 }
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
 #==================================================================================================#
 #' @export
 #use_labels
 use_labels <- function(dt, keep = NULL, drop = NULL) {
 
-  labels <- codebook$labels
+  labels <- get(paste0('nhts_',attr(dt, 'dataset')))[['labels']]
   
   if(!is.null(keep)) {
 
@@ -61,9 +85,27 @@ format_values <- function(x, digits = 2, percentage = FALSE, scientific = FALSE,
   x <- trimws(x)
   return(x)
 }
+
 #==================================================================================================#
 #' @export
 #crosstab_output
 crosstab_output <- function(W = 'Weighted', E = 'Std. Error', S = 'Surveyed') {
   c(W = W, E = E, S = S)
 }
+
+#==================================================================================================#
+#' @export
+get_trip_weights <- function(data) {
+  person_weights <- copy(data$weights$person)
+  person_weight_names <- WGT('person')
+  trip_weight_names <- WGT('trip')
+  person_weights[, (person_weight_names) := lapply(.SD, function(x) x * 365), .SDcols = person_weight_names]
+  setnames(person_weights, person_weight_names, trip_weight_names)
+  setkeyv(person_weights, c(ID('household'), ID('person')))
+  trip_weights <- merge(copy(data$weights$trip_keys), person_weights)
+  setkeyv(trip_weights, c(ID('household'), ID('person'), ID('trip')))
+  return(trip_weights)
+}
+
+
+
