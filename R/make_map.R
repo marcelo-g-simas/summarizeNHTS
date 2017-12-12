372ff8e40b785e086dcbdcf3bad5abe017b515d7
cates_a <alexandercates@westat.com>
2017-12-12 13:11:45 -0500

    new configurations for make_map function

--- R/make_map.R
+++ R/make_map.R
@@ -13,56 +13,67 @@ NULL
 #' @return ggiraph/htmlwidget class object
 #' @examples
 #' @export
-make_map <- function(table1, table2, state_style = "normal", digits = 2, percentage = attr(table1, "prop"), scientific = F, multiplier = NULL) {
+make_map <- function(tbl, table2, state_style = "normal", ...) {
 
-  if(missing(table1)) {
-    stop("table1 parameter is not set")
+  dataset <- attr(tbl, 'dataset')
+  values <- CB(dataset)$values
+  group_var <- attr(tbl, 'by')
+  
+  format_map_values <- function(x) {
+    do.call(format_values, c(list(x = x), list(...)))
   }
-  if(!("dataset" %in% names(attributes(table1)))) {
-    stop("table1 does not appear to be a table returned by make_table()")
+
+  if(length(group_var) > 1) {
+    stop('tbl parameter has too many "by" variables.',
+         '\ntbl can only be a single geography group variable (one value per geography record)',
+         '\nConsider using table2 parameter feature if you want to further group geography level data')
   }
   
-  if(!any(attributes(table1)$by %in% c("HHSTATE","HH_CBSA"))) {
-    stop("table1 must have a single geography group variable, either HHSTATE (state) or HH_CBSA (city)")
-  }
-  
-  if(length(attributes(table1)$by) > 1) {
-    stop("table1 has too many group variables. table1 can only be a single geography group variable (one value per geography record) - consider using table2 parameter feature if you want to further group geography level data")
-  }
-  
-  if("HHSTATE" %in% attributes(table1)$by) {
-    if(!state_style %in% c("normal", "tile")) {
-      stop("state_style parameter must be either 'normal' or 'tile'") 
-    }
-    if(state_style=="normal") {
-      geography <- "state"
-    }
-    if(state_style=="tile") {
-      geography <- "state_tile"
+  choose_state_layer <- function(x) {
+    if (x == 'normal') {
+      state_layer
+    } else if (x == 'tile') {
+      state_tile_layer
+    } else {
+      stop('state_style parameter must be either "normal" or "tile"')
     }
   }
   
-  if("HH_CBSA" %in% attributes(table1)$by) {
-    geography <- "cbsa" 
+  geo_layer <- switch(group_var,
+    'HHSTFIPS' = choose_state_layer(state_style),
+    'HH_CBSA' = cbsa_layer,
+    'CENSUS_R' = census_region_layer,
+    'CENSUS_D' = census_division_layer
+  )
+  
+  if (is.null(geo_layer)) {
+    stop(sprintf('Table has incompatible "by" variable: %s', group_var))
   }
   
-  if(geography == "state") {
-    geog_var <- "HHSTATE"
-    merged <- merge(x = state_layer, y = table1, by.x = "STUSPS", by.y = geog_var, all.x = TRUE)
+  if (attr(tbl,'label') == TRUE) {
+    # If value labels were used, lookup value in codebook
+    labeled_tbl <- merge(
+      x = tbl, 
+      y = values[NAME == group_var, list(VALUE, LABEL)], 
+      by.x = group_var, 
+      by.y = 'LABEL', 
+      all.x = TRUE
+    )
+    by_y <- 'VALUE'
+  } else {
+    labeled_tbl <- tbl
+    by_y <- group_var
   }
-  if(geography == "state_tile") {
-    geog_var <- "HHSTATE"
-    merged <- merge(x = state_tile_layer, y = table1, by.x = "STUSPS", by.y = geog_var, all.x = TRUE)
-  }
-  if(geography == "cbsa") {
-    geog_var <- "HH_CBSA"
-    cbsa_layer$idx <- row.names(cbsa_layer) # stash index order because merge does not preserve order, important when mapped by geom_etc.
-    merged <- merge(x = cbsa_layer, y = table1, by.x = "CBSAFP", by.y = geog_var, all.x = TRUE)
-    merged <- merged[order(merged$idx), ]
-    merged$idx <- NULL
-  }
-  colnames(merged)[1] <- geog_var
   
+  # Merge table with geography layer
+  merged <- merge(
+    x = geo_layer, 
+    y = labeled_tbl, 
+    by.x = 'GEOID', 
+    by.y = by_y, 
+    all.x = TRUE
+  )
+ 
   if(!missing(table2)) {
       
     if(!("dataset" %in% names(attributes(table2)))) {
@@ -105,14 +116,7 @@ make_map <- function(table1, table2, state_style = "normal", digits = 2, percent
     
     # Create formatted copy of table for the tooltip
     formatted_tbl <- copy(merged[, c("W", "E")])
-    formatted_tbl <- lapply(
-      X = formatted_tbl, 
-      FUN = format_values, 
-      digits = digits,
-      percentage = percentage,
-      scientific = scientific,
-      multiplier = multiplier
-    )
+    formatted_tbl <- lapply(X = formatted_tbl, FUN = format_map_values)
     
     # Create tooltip
     merged$tooltip <- sprintf('%s &plusmn; %s', 
@@ -124,8 +128,10 @@ make_map <- function(table1, table2, state_style = "normal", digits = 2, percent
   
   merged$NAME <- gsub("'", "&apos;", merged$NAME)
   
-  geog_layer <- geom_polygon_interactive(
+  gglayer <- geom_polygon_interactive(
     data = merged,
+    color = "#FFFFFF",
+    size = 0.35,
     mapping = aes(
       x = long,
       y = lat,
@@ -133,37 +139,59 @@ make_map <- function(table1, table2, state_style = "normal", digits = 2, percent
       fill = W,
       tooltip = paste0("<b>", NAME, "</b>", "<br>", tooltip),
       data_id = group
-      ),
-    color = "#FFFFFF",
-    size = 0.35)
+    )
+  )
   
   # if not mapping state level data, at least provide state border as frame of reference
-  if(geography %in% c("cbsa")) {
-    state_border <- geom_polygon_interactive(data=state_layer, mapping = aes(x = long, y = lat, group = group), color = "#999999", size = 0.35, fill = NA)
+  if (attr(geo_layer, 'layer_name') == 'cbsa_layer') {
+    state_border <- geom_polygon_interactive(
+      data = state_layer, 
+      mapping = aes(
+        x = long,
+        y = lat,
+        group = group
+      ), 
+      color = "#999999", 
+      size = 0.35, 
+      fill = NA
+    )
   } else {
     state_border <- NULL
   }
   
   # put a geom_text label in the center of the polygon
-  if(geography %in% c("state_tile")) {
-  	label_centroid <- data.frame(STUSPS=sort(unique(state_tile_layer$STUSPS)), long=aggregate(long~STUSPS, state_tile_layer, mean)[, 2], lat=aggregate(lat~STUSPS, state_tile_layer, mean)[, 2])
-  	label_text <- geom_text_interactive(data=label_centroid, aes(label = STUSPS, x = long, y = lat))
+  if (attr(geo_layer, 'layer_name') == 'state_tile_layer') {
+    label_centroid <- merge(state_tile_layer, values[NAME == 'HHSTATE'], by.x = 'NAME', by.y = 'LABEL')
+  	label_centroid <- label_centroid[, list(long = mean(long), lat = mean(lat)), by = VALUE]
+  	label_text <- geom_text_interactive(data = label_centroid, aes(label = VALUE, x = long, y = lat))
   } else {
   	label_text <- NULL
   }
   
+  
+  
   map <- ggplot() +
     state_border +
-    geog_layer +
+    gglayer +
     label_text +
     coord_fixed() + 
     theme_void() + 
-    scale_fill_gradient(low = "#f2f0f7", high = "#54278f", labels = function(x) { format(x, big.mark = ",", scientific = scientific, trim = TRUE) })
+    scale_fill_gradient(
+      low = "#f2f0f7", 
+      high = "#54278f", 
+      labels = format_map_values
+    )
   
   tooltip_css <- "background-color:#F2F2F2; padding:10px; border-radius:10px 20px 10px 20px"
   tooltip_css <- ifelse(!missing(table2), paste0(tooltip_css, "; width:400px"), tooltip_css) # give sensible fixed width to tooltips with charts
   
-  map_widget <- ggiraph(code = {print(map)}, tooltip_extra_css = tooltip_css, hover_css = "border:0; fill:#fff7bc", width_svg = 8, height_svg = 8)
-  map_widget
+  map_widget <- ggiraph(
+    code = {print(map)}, 
+    tooltip_extra_css = tooltip_css, 
+    hover_css = "border:0; fill:#fff7bc", 
+    width_svg = 8, height_svg = 8
+  )
+  
+  return(map_widget)
   
 }
